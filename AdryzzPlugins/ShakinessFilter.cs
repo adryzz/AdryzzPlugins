6cbcd52c1e360974a906467eda037b1ea6a3b7b1
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-01-27 11:33:42 +0100

    Add project files.

---
+++ AdryzzPlugins/ShakinessFilter.cs
@@ -0,0 +1,36 @@
+﻿using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using System;
+using System.Numerics;
+
+namespace AdryzzPlugins
+{
+    [PluginName("Shakiness Filter")]
+    public class ShakinessFilter : Notifier, IFilter
+    {
+        public FilterStage FilterStage => FilterStage.PostTranspose;
+
+        DateTime start = DateTime.Now;
+
+        public Vector2 Filter(Vector2 point)
+        {
+            if (DateTime.Now.Subtract(start).TotalMilliseconds >= Response)
+            {
+                Random r = new Random();
+                start = DateTime.Now;
+                return new Vector2(point.X + r.Next(-RngX, RngX), point.Y + r.Next(-RngY, RngY));
+            }
+            return point;
+        }
+
+        [Property("X distance"), Unit("px")]
+        public int RngX { get; set; } = 20;
+
+        [Property("Y distance"), Unit("px")]
+        public int RngY { get; set; } = 20;
+
+        [Property("Shakiness"), Unit("ms")]
+        public int Response { get; set; } = 5;
+    }
+}
---
+++ AdryzzPlugins/VariableAreaTool.cs
@@ -0,0 +1,22 @@
+﻿using OpenTabletDriver.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdryzzPlugins
+{
+    class VariableAreaTool : ITool
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public bool Initialize()
+        {
+            return true;
+        }
+    }
+}
